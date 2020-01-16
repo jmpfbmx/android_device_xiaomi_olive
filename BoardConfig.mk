537f9f7bcae525581737724dad97bcc47381ba1e
zarco200 <fightstart20000@yahoo.com>
2020-01-16 06:55:09 -0500

    added more files from common tree into device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,84 +14,56 @@
 # limitations under the License.
 #
 
+# Inherit from common sdm439-common
+-include device/xiaomi/olive/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/olive
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_USES_64_BIT_BINDER := true
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
 
-# Board
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := MSM8937
-TARGET_BOARD_PLATFORM := msm8937
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_CPU_CORTEX_A53 := true
 
-# Dex-preopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
-
-# Display
-TARGET_USES_GRALLOC1 := true
-TARGET_USES_HWC2 := true
-
-# Encryption
-TARGET_HW_DISK_ENCRYPTION := true
-
-# Kernel
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8
-BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true androidboot.selinux=permissive androidboot.configfs=true
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image  loop.max_part=7
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_BOOTIMG_HEADER_VERSION := 1
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-TARGET_KERNEL_VERSION := 4.9
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/xiaomi/olive
-TARGET_KERNEL_CONFIG := olive-perf_defconfig
-#TARGET_KERNEL_CLANG_COMPILE := true
-
-# OTA
+# Assert
 TARGET_OTA_ASSERT_DEVICE := olive
 
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# HIDL
+#DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
+#DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+#DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+#DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
+# Kernel
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78B0000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
+TARGET_KERNEL_CONFIG := lolz_olive_defconfig
+
+# Partition
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4831838208
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 53678685696
-BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1610612736
 
-# SELinux
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
-
-# Qualcomm support
-BOARD_USES_QCOM_HARDWARE := true
+# DRM
+TARGET_ENABLE_MEDIADRM_64 := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/twrp.fstab
-TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
-# Verity
-BOARD_AVB_ENABLE := false
-BOARD_BUILD_DISABLED_VBMETA_IMAGE := true
+# Security patch level
+VENDOR_SECURITY_PATCH := 2019-11-01
 
-# VNDK
-BOARD_VNDK_VERSION := current
-
-# Inherit proprietary version
+# Inherit from the proprietary version
 -include vendor/xiaomi/olive/BoardConfigVendor.mk
