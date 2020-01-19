0a3f99ef69102f0e2f603f213ac9a61312294c22
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2020-01-19 21:16:32 +0100

    Restructuring lineage_olive.mk

--- lineage_olive.mk
+++ lineage_olive.mk
@@ -1,3 +1,18 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -8,16 +23,22 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from olive device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_BRAND := xiaomi
+# Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := olive
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_NAME := lineage_olive
+PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi 8
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_MANUFACTURER := xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := olive
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="olive-user 9 PKQ1.190319.001 V11.0.1.0.PCNINXM release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=olive \
+   PRIVATE_BUILD_DESC="olive-user 9 PKQ1.190319.001 V11.0.1.0.PCNINXM release-keys"
+
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/olive/olive:9/PKQ1.190319.001/V11.0.1.0.PCNINXM:user/release-keys
